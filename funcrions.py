5b61b136df09577d76e61e2149ac201a6908779c
Sshmanov <92789156+Sshmanov@users.noreply.github.com>
2024-08-26 13:08:36 +0500

    Add files via upload

---
+++ funcrions.py
@@ -0,0 +1,36 @@
+def read_sales_data(file_path):
+    sales = {'product_name': [], 'quantity': [], 'price': [], 'date': []}
+    with open(file_path, 'r', encoding='utf-8') as my_file:
+        for line in my_file:
+            product_name, quantity, price, date = line.strip().split(',')
+            quantity = int(quantity)
+            price = int(price)
+            sales['product_name'].append(product_name)
+            sales['quantity'].append(quantity)
+            sales['price'].append(price)
+            sales['date'].append(date.strip())
+    return sales
+
+def total_sales_per_product(sales):
+    total = {}
+    for i, x in enumerate(sales['product_name']):
+        if x in total:
+            total[x] += sales['quantity'][i] * sales['price'][i]
+        else:
+            total[x] = sales['quantity'][i] * sales['price'][i]
+    return total
+
+def sales_over_time(sales):
+    total = {}
+    for i, x in enumerate(sales['date']):
+        if x in total:
+            total[x] += sales['quantity'][i] * sales['price'][i]
+        else:
+            total[x] = sales['quantity'][i] * sales['price'][i]
+    return total
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+import funcrions
+import matplotlib.pyplot as plt
+
+
+path = input('Path to file: \n')
+sales = funcrions.read_sales_data(path)
+items = funcrions.total_sales_per_product(sales)
+dates = funcrions.sales_over_time(sales)
+
+max_items = max(items, key=items.get)
+print(f'Продукт с наибольшей выручкой  {max_items}')
+#print(f'Дата с наибольшей выручкой - {max_items}')
+
+plt.bar(x=items.keys(), height=items.values())
