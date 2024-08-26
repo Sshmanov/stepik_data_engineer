1edcc2d966d5632f1423fdb0e49e48c5f54ca572
Sshmanov <92789156+Sshmanov@users.noreply.github.com>
2024-08-26 13:24:34 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import funcrions
 import matplotlib.pyplot as plt
 
-
 path = input('Path to file: \n')
 sales = funcrions.read_sales_data(path)
 items = funcrions.total_sales_per_product(sales)
 dates = funcrions.sales_over_time(sales)
 
 max_items = max(items, key=items.get)
-print(f'Продукт с наибольшей выручкой  {max_items}')
-print(f'Дата с наибольшей выручкой - {max_items}')
+print(f'РџСЂРѕРґСѓРєС‚ СЃ РЅР°РёР±РѕР»СЊС€РµР№ РІС‹СЂСѓС‡РєРѕР№ - {max_items}')
+print(f'Р”Р°С‚Р° СЃ РЅР°РёР±РѕР»СЊС€РµР№ РІС‹СЂСѓС‡РєРѕР№ - {max_items}')
 
 plt.bar(x=items.keys(), height=items.values())
 plt.bar(x=dates.keys(), height=dates.values())
