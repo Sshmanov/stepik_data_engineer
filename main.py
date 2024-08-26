c7f8f05c27864cb369c1fcbf3c82c2970b173daf
Sshmanov <92789156+Sshmanov@users.noreply.github.com>
2024-08-26 13:47:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ items = funcrions.total_sales_per_product(sales)
 dates = funcrions.sales_over_time(sales)
 
 max_items = max(items, key=items.get)
-print(f'Продукт с наибольшей выручкой - {max_items}')
-print(f'Дата с наибольшей выручкой - {max_items}')
+max_dates = max(dates, key=dates.get)
 
+print(f'Продукт с наибольшей выручкой - {max_items}')
+print(f'Дата с наибольшей выручкой - {max_dates}')
+
+plt.figure(figsize=(10,6))
 plt.bar(x=items.keys(), height=items.values())
+plt.title('Выручка по товарам')
+plt.show()
+plt.figure(figsize=(10,6))
 plt.bar(x=dates.keys(), height=dates.values())
+plt.title('Выручка по датам')
+plt.show()
